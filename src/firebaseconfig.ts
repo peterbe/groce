7991c30214f2ce647ef496614a3b1c3574c18619
Peter Bengtsson <mail@peterbe.com>
2020-08-24 09:24:18 -0400

    new optional firebaseconfig key

--- src/firebaseconfig.ts
+++ src/firebaseconfig.ts
@@ -6,14 +6,18 @@ const KEYS = [
   "storageBucket",
   "messagingSenderId",
   "appId",
+  "measurementId",
 ];
+
+const OPTIONAL = new Set(["measurementId"]);
 export const firebaseConfig = fromEntries(
   KEYS.map((key) => {
     const envKey = `PREACT_APP_FIREBASE_${key.toUpperCase()}`;
     const value = process.env[envKey];
-    if (!value) {
-      const hint = 'In your .env file, create something like:\n\n' +
-        `${envKey}=…value…\n\n`
+    if (!value && !OPTIONAL.has(key)) {
+      const hint =
+        "In your .env file, create something like:\n\n" +
+        `${envKey}=…value…\n\n`;
       throw new Error(`Expected ${envKey} to be set to something.\n${hint}`);
     }
     return [key, value];
@@ -22,7 +26,7 @@ export const firebaseConfig = fromEntries(
 
 function fromEntries<T>(iterable: Array<[string, T]>): { [key: string]: T } {
   return [...iterable].reduce<{ [key: string]: T }>((obj, [key, val]) => {
-    obj[key] = val
-    return obj
-  }, {})
+    obj[key] = val;
+    return obj;
+  }, {});
 }
