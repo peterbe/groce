20c29e1b0fd4dba565df301fd8335b03193fed61
Peter Bengtsson <mail@peterbe.com>
2020-08-31 13:04:43 -0400

    delay offline message

--- src/components/offline-warning/index.tsx
+++ src/components/offline-warning/index.tsx
@@ -1,10 +1,34 @@
 import { FunctionalComponent, h } from "preact";
+import { useState, useEffect } from "preact/hooks";
+
 import * as style from "./style.css";
 
-export const OfflineWarning: FunctionalComponent = () => {
-  return (
-    <p class={style.offline_warning}>
-      Some changes stuck offline. Keep the app open to sync!
-    </p>
-  );
+interface Props {
+  delay?: number;
+  text?: string;
+}
+export const OfflineWarning: FunctionalComponent<Props> = ({
+  delay = 1,
+  text = "Some changes stuck offline. Keep the app open to sync!",
+}: Props) => {
+  const [waited, setWaited] = useState(delay === 0);
+  useEffect(() => {
+    let mounted = true;
+    const timer = setTimeout(() => {
+      if (mounted) {
+        setWaited(true);
+      }
+    }, delay * 1000);
+    return () => {
+      mounted = false;
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [delay]);
+  if (!waited) {
+    return null;
+  }
+
+  return <p class={style.offline_warning}>{text}</p>;
 };
