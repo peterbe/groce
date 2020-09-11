f691d56d1b2c856418e09636c55f659b82acf5fd
Peter Bengtsson <mail@peterbe.com>
2020-09-11 11:00:57 -0400

    postmark on feedback

--- src/components/alerts.tsx
+++ src/components/alerts.tsx
@@ -21,34 +21,32 @@ export const Alert: FunctionalComponent<Props> = ({
       <h4 class="alert-heading">{heading}</h4>
       {message instanceof Error ? message.toString() : message}
       <hr />
-      <div class="row justify-content-between">
-        <div class="col-4">
-          <Link href="/" class="btn btn-outline-primary">
-            Go back to <b>home page</b>
-          </Link>
-        </div>
-        <div class="col-4">
-          {offerReload && (
-            <button
-              type="button"
-              class="btn btn-danger"
-              onClick={() => {
-                setReloading(true);
-                window.location.reload(true);
-              }}
-            >
-              {reloading && (
-                <span
-                  class="spinner-grow spinner-grow-sm"
-                  role="status"
-                  aria-hidden="true"
-                ></span>
-              )}
-              {reloading ? " Reloading..." : "Try reloading"}
-            </button>
-          )}
-        </div>
-      </div>
+      <p>
+        <Link href="/" class="btn btn-outline-primary">
+          Go back to <b>home page</b>
+        </Link>
+      </p>
+      {offerReload && (
+        <p>
+          <button
+            type="button"
+            class="btn btn-danger"
+            onClick={() => {
+              setReloading(true);
+              window.location.reload(true);
+            }}
+          >
+            {reloading && (
+              <span
+                class="spinner-grow spinner-grow-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>
+            )}
+            {reloading ? " Reloading..." : "Try reloading"}
+          </button>
+        </p>
+      )}
     </div>
   );
 };
