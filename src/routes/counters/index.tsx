e9be6c8fd62f8295eff6f9937c418b99a315b0bd
Peter Bengtsson <mail@peterbe.com>
2020-10-09 22:08:41 -0400

    Graph counters (#34)
    * cleanup
    
    * finishing graph counters

--- src/routes/counters/index.tsx
+++ src/routes/counters/index.tsx
@@ -1,39 +1,20 @@
 import { FunctionalComponent, h } from "preact";
 import * as style from "./style.css";
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
+import rv from "rough-viz/dist/roughviz.min";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import { GoBack } from "../../components/go-back";
 import { Loading } from "../../components/loading";
+
+dayjs.extend(relativeTime);
 
 interface Props {
   db: firebase.firestore.Firestore | null;
 }
 
 type Counter = { [id: string]: number };
-
-// function getYesterday() {
-//   const date = new Date();
-//   date.setDate(date.getDate() - 1);
-//   return date;
-// }
-
-// function getLastMonth() {
-//   const date = new Date();
-//   const currentMonth = date.getMonth();
-//   while (currentMonth === date.getMonth()) {
-//     date.setDate(date.getDate() - 1);
-//   }
-//   return date;
-// }
-
-// function getLastYear() {
-//   const date = new Date();
-//   const currentYear = date.getFullYear();
-//   while (currentYear === date.getFullYear()) {
-//     date.setDate(date.getDate() - 28);
-//   }
-//   return date;
-// }
 
 const Counters: FunctionalComponent<Props> = (props: Props) => {
   const { db } = props;
@@ -51,18 +32,6 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
   const thisMonth = `${year}-${month}`;
   const thisDay = `${year}-${month}-${day}`;
 
-  // const yesterday = getYesterday();
-  // const previousDaySplit = yesterday.toISOString().split("T")[0].split("-");
-  // const previousDay = `${previousDaySplit[0]}-${previousDaySplit[1]}-${previousDaySplit[2]}`;
-
-  // const lastMonth = getLastMonth();
-  // const lastMonthSplit = lastMonth.toISOString().split("T")[0].split("-");
-  // const previousMonth = `${lastMonthSplit[0]}-${lastMonthSplit[1]}`;
-
-  // const lastYear = getLastYear();
-  // const lastYearSplit = lastYear.toISOString().split("T")[0].split("-");
-  // const previousYear = lastYearSplit[0];
-
   useEffect(() => {
     if (db) {
       db.collection("counters")
@@ -71,16 +40,10 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
           (snapshot) => {
             const data = snapshot.data() as Counter;
             const newItemsDone = new Map<string, number>();
-            Object.entries(data).forEach(([key, value]) =>
-              newItemsDone.set(key, value)
-            );
-            // newItemsDone.set("ever", data["ever"] || 0);
-            // newItemsDone.set(thisYear, data[thisYear] || 0);
-            // // newItemsDone.set(previousYear, data[previousYear]);
-            // newItemsDone.set(thisMonth, data[thisMonth] || 0);
-            // // newItemsDone.set(previousMonth, data[previousMonth]);
-            // newItemsDone.set(thisDay, data[thisDay] || 0);
-            // // newItemsDone.set(previousDay, data[previousDay]);
+            Object.entries(data).forEach(([key, value]) => {
+              newItemsDone.set(key, value);
+            });
+
             setItemsDone(newItemsDone);
           },
           (error) => {
@@ -96,13 +59,7 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
             Object.entries(data).forEach(([key, value]) =>
               newListsCreated.set(key, value)
             );
-            // newListsCreated.set("ever", data["ever"] || 0);
-            // newListsCreated.set(thisYear, data[thisYear] || 0);
-            // // newListsCreated.set(previousYear, data[previousYear]);
-            // newListsCreated.set(thisMonth, data[thisMonth] || 0);
-            // // newListsCreated.set(previousMonth, data[previousMonth]);
-            // newListsCreated.set(thisDay, data[thisDay] || 0);
-            // // newListsCreated.set(previousDay, data[previousDay]);
+
             setListsCreated(newListsCreated);
           },
           (error) => {
@@ -115,8 +72,6 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
   if (!itemsDone.size || !listsCreated.size) {
     return <Loading text="Loading numbers..." />;
   }
-
-  // console.log(itemsDone);
 
   return (
     <div>
@@ -159,7 +114,8 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
         </tbody>
       </table>
 
-      {/* <h4>All recent numbers</h4> */}
+      <h3>All recent numbers</h3>
+      <DayByDayNumberTable map={itemsDone} title="Items done" />
     </div>
   );
 };
@@ -177,6 +133,79 @@ function TDRow({
       <span class={style.number}>{value}</span>
     </td>
   );
+}
+
+let lastId = 0;
+
+const generateId = (prefix: string) => `${prefix}${++lastId}`;
+
+function DayByDayNumberTable({
+  map,
+  title,
+  maxDays = 11,
+}: {
+  maxDays?: number;
+  title: string;
+  map: Map<string, number>;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [id] = useState(generateId("roughviz"));
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      const labels: string[] = [];
+      const values: number[] = [];
+      const date = new Date();
+      const toKey = (d: Date) => d.toISOString().split("T")[0];
+      const today = dayjs();
+      while (labels.length < maxDays) {
+        const value = map.get(toKey(date)) || 0;
+        const thisDate = dayjs(date);
+        const diffDates = today.diff(thisDate, "day");
+
+        labels.push(
+          diffDates === 0
+            ? "today"
+            : diffDates === 1
+            ? "yesterday"
+            : today.to(date)
+        );
+        values.push(value);
+        date.setDate(date.getDate() - 1);
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      new rv.BarH({
+        element: `#${id}`,
+        title,
+        titleFontSize: "1.5rem",
+        legend: false,
+        // margin: { top: 50, bottom: 100, left: 160, right: 0 },
+        data: {
+          labels,
+          values,
+        },
+        // xLabel: "Number",
+        strokeWidth: 1,
+        fillStyle: "zigzag-line",
+        highlight: "gold",
+        roughness: 1,
+        width: Math.min(
+          1000,
+          document.querySelector("#app.container")?.clientWidth || 1000
+        ),
+      });
+    }
+    return () => {
+      if (node) {
+        while (node.firstChild) {
+          node.removeChild(node.firstChild);
+        }
+      }
+    };
+  }, [map, id, title, maxDays]);
+
+  return <div id={id} ref={ref} />;
 }
 
 const CountersOuter: FunctionalComponent<Props> = (props: Props) => {
