f12dd9910a84af7838f6cf73e34afa12324303e7
Peter Bengtsson <mail@peterbe.com>
2020-10-09 21:48:24 -0400

    finishing graph counters

--- src/routes/counters/index.tsx
+++ src/routes/counters/index.tsx
@@ -16,30 +16,6 @@ interface Props {
 
 type Counter = { [id: string]: number };
 
-// function getYesterday() {
-//   const date = new Date();
-//   date.setDate(date.getDate() - 1);
-//   return date;
-// }
-
-// function getLastMonth() {
-//   const date = new Date();
-//   const currentMonth = date.getMonth();
-//   while (currentMonth === date.getMonth()) {
-//     date.setDate(date.getDate() - 1);
-//   }
-//   return date;
-// }
-
-// function getLastYear() {
-//   const date = new Date();
-//   const currentYear = date.getFullYear();
-//   while (currentYear === date.getFullYear()) {
-//     date.setDate(date.getDate() - 28);
-//   }
-//   return date;
-// }
-
 const Counters: FunctionalComponent<Props> = (props: Props) => {
   const { db } = props;
   useEffect(() => {
@@ -56,18 +32,6 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
   const thisMonth = `${year}-${month}`;
   const thisDay = `${year}-${month}-${day}`;
 
-  // const yesterday = getYesterday();
-  // const previousDaySplit = yesterday.toISOString().split("T")[0].split("-");
-  // const previousDay = `${previousDaySplit[0]}-${previousDaySplit[1]}-${previousDaySplit[2]}`;
-
-  // const lastMonth = getLastMonth();
-  // const lastMonthSplit = lastMonth.toISOString().split("T")[0].split("-");
-  // const previousMonth = `${lastMonthSplit[0]}-${lastMonthSplit[1]}`;
-
-  // const lastYear = getLastYear();
-  // const lastYearSplit = lastYear.toISOString().split("T")[0].split("-");
-  // const previousYear = lastYearSplit[0];
-
   useEffect(() => {
     if (db) {
       db.collection("counters")
@@ -76,16 +40,10 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
           (snapshot) => {
             const data = snapshot.data() as Counter;
             const newItemsDone = new Map<string, number>();
-            Object.entries(data).forEach(([key, value]) =>
-              newItemsDone.set(key, value)
-            );
-            // newItemsDone.set("ever", data["ever"] || 0);
-            // newItemsDone.set(thisYear, data[thisYear] || 0);
-            // // newItemsDone.set(previousYear, data[previousYear]);
-            // newItemsDone.set(thisMonth, data[thisMonth] || 0);
-            // // newItemsDone.set(previousMonth, data[previousMonth]);
-            // newItemsDone.set(thisDay, data[thisDay] || 0);
-            // // newItemsDone.set(previousDay, data[previousDay]);
+            Object.entries(data).forEach(([key, value]) => {
+              newItemsDone.set(key, value);
+            });
+
             setItemsDone(newItemsDone);
           },
           (error) => {
@@ -101,13 +59,7 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
             Object.entries(data).forEach(([key, value]) =>
               newListsCreated.set(key, value)
             );
-            // newListsCreated.set("ever", data["ever"] || 0);
-            // newListsCreated.set(thisYear, data[thisYear] || 0);
-            // // newListsCreated.set(previousYear, data[previousYear]);
-            // newListsCreated.set(thisMonth, data[thisMonth] || 0);
-            // // newListsCreated.set(previousMonth, data[previousMonth]);
-            // newListsCreated.set(thisDay, data[thisDay] || 0);
-            // // newListsCreated.set(previousDay, data[previousDay]);
+
             setListsCreated(newListsCreated);
           },
           (error) => {
@@ -120,8 +72,6 @@ const Counters: FunctionalComponent<Props> = (props: Props) => {
   if (!itemsDone.size || !listsCreated.size) {
     return <Loading text="Loading numbers..." />;
   }
-
-  // console.log(itemsDone);
 
   return (
     <div>
@@ -192,7 +142,7 @@ const generateId = (prefix: string) => `${prefix}${++lastId}`;
 function DayByDayNumberTable({
   map,
   title,
-  maxDays = 12,
+  maxDays = 11,
 }: {
   maxDays?: number;
   title: string;
@@ -208,7 +158,7 @@ function DayByDayNumberTable({
       const date = new Date();
       const toKey = (d: Date) => d.toISOString().split("T")[0];
       const today = dayjs();
-      while (map.has(toKey(date)) && labels.length < maxDays) {
+      while (labels.length < maxDays) {
         const value = map.get(toKey(date)) || 0;
         const thisDate = dayjs(date);
         const diffDates = today.diff(thisDate, "day");
@@ -236,10 +186,14 @@ function DayByDayNumberTable({
           values,
         },
         // xLabel: "Number",
-        strokeWidth: 2,
+        strokeWidth: 1,
         fillStyle: "zigzag-line",
         highlight: "gold",
         roughness: 1,
+        width: Math.min(
+          1000,
+          document.querySelector("#app.container")?.clientWidth || 1000
+        ),
       });
     }
     return () => {
