5f677f457e4c1e45de8019fbb2c6a1cec75f548c
Peter Bengtsson <mail@peterbe.com>
2020-10-21 12:20:27 -0400

    specify return type on stripEmojis

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,4 @@
-export function stripEmojis(s: string) {
+export function stripEmojis(s: string): string {
   return s
     .replace(
       /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
