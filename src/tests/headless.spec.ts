c1234df9c8f8c0b0de18a911e6c8ce91cd809062
Peter Bengtsson <mail@peterbe.com>
2021-09-17 10:14:41 -0400

    improvements

--- src/tests/headless.spec.ts
+++ src/tests/headless.spec.ts
@@ -1,10 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('basic test', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+const testURL = (pathname: string) => `http://localhost:8080${pathname}`;
+
+test("homepage anonymous", async ({ page }) => {
+  await page.goto(testURL("/"));
   await expect(page).toHaveTitle("That's Groce!");
-  const title = page.locator('h1');
+  const title = page.locator("h1");
   await expect(title).toHaveText("That's Groce!");
+});
 
-  await page.click('text=Get started without signing in');
+test("other pages anonymous", async ({ page }) => {
+  await page.goto(testURL("/about"));
+
+  await expect(page).toHaveTitle("About this app");
+  const title = page.locator("h1");
+  await expect(title).toHaveText("About this app");
+  await page.locator("text=← Back to app").first().click();
+  expect(page.url()).toBe(testURL("/"));
+});
+
+test("basics as guest", async ({ page }) => {
+  await page.goto(testURL("/"));
+  await page.click("text=Get started without signing in");
+  await expect(
+    page.locator("text=temporarily signed in").first()
+  ).toBeVisible();
+  await page.click('a:has-text("Groceries")');
+
+  await expect(
+    page.locator("text=List is empty at the moment.").first()
+  ).toBeVisible();
+
+  await page.fill('input[aria-label="New shopping list item"]', "Carrots");
+
+  page.locator('button[type="submit"]').first().click();
+  await expect(page.locator("text=Carrots 🥕").first()).toBeVisible();
+  // await page.pause();
 });
