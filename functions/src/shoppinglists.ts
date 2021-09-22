f300fb9cad8628312a87e53ae6a891b92f67e341
Peter Bengtsson <mail@peterbe.com>
2021-09-21 22:34:29 -0400

    improvements to onShoppinglist... functions

--- functions/src/shoppinglists.ts
+++ functions/src/shoppinglists.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
+import { wrappedLogError } from "./rollbar-logger";
+
 interface BriefItem {
   text: string;
   description: string;
@@ -11,109 +13,95 @@ interface BriefItem {
 
 export const onShoppinglistItemWrite = functions.firestore
   .document("shoppinglists/{listID}/items/{itemID}")
-  .onWrite((change, context) => {
-    // Number of items to put into the `recent_items` array.
-    // It's ideal if this matches the business logic of displaying.
-    const CUTOFF_RECENT_ITEMS = 5;
+  .onWrite(
+    wrappedLogError(async (change, context): Promise<void> => {
+      // Number of items to put into the `recent_items` array.
+      // It's ideal if this matches the business logic of displaying.
+      const CUTOFF_RECENT_ITEMS = 5;
 
-    return admin
-      .firestore()
-      .collection("shoppinglists")
-      .doc(context.params.listID)
-      .get()
-      .then((doc) => {
-        if (!doc.exists) {
-          return Promise.resolve("Shopping list does not exist :(");
-        }
+      const { listID } = context.params;
+      const doc = await admin
+        .firestore()
+        .collection("shoppinglists")
+        .doc(listID)
+        .get();
+
+      if (!doc.exists) {
+        functions.logger.warn(`Shopping list ${listID} does not exist`);
+        return;
+      }
+      const data = doc.data();
+      if (!data) {
+        functions.logger.warn(
+          `Shopping list ${listID} has no data (deletion?)`
+        );
+        return;
+      }
+      const recentItemsBefore = data.recent_items;
+      const activeItemsCountBefore = data.active_items_count;
+
+      // Get a summary of the top 10 most recently added items
+      const snapshot = await admin
+        .firestore()
+        .collection("shoppinglists")
+        .doc(context.params.listID)
+        .collection("items")
+        .where("removed", "==", false)
+        .get();
+
+      const items: BriefItem[] = [];
+      snapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log("ITEM...", doc.id, " => ", doc.data());
         const data = doc.data();
-        if (!data) {
-          return Promise.resolve("Shopping list contains no data :(");
-        }
-        const recentItemsBefore = data.recent_items;
-        const activeItemsCountBefore = data.active_items_count;
-
-        // Get a summary of the top 10 most recently added items
-        return admin
-          .firestore()
-          .collection("shoppinglists")
-          .doc(context.params.listID)
-          .collection("items")
-          .where("removed", "==", false)
-          .get()
-          .then((snapshot) => {
-            const items: BriefItem[] = [];
-            snapshot.forEach((doc) => {
-              // doc.data() is never undefined for query doc snapshots
-              // console.log("ITEM...", doc.id, " => ", doc.data());
-              const data = doc.data();
-              items.push({
-                text: data.text,
-                description: data.description,
-                added: data.added[0].toDate(),
-                quantity: data.quantity || 0,
-                done: data.done,
-              });
-            });
-            items.sort((a, b) => {
-              if (a.done === b.done) {
-                return b.added.getTime() - a.added.getTime();
-              } else if (a.done) {
-                return 1;
-              } else {
-                return -1;
-              }
-            });
-
-            const recentItems = items
-              .slice(0, CUTOFF_RECENT_ITEMS)
-              .map((item) => {
-                return {
-                  text: item.text,
-                  description: item.description,
-                  quantity: item.quantity,
-                  done: item.done,
-                };
-              });
-
-            if (
-              items.length === activeItemsCountBefore &&
-              JSON.stringify(recentItemsBefore) === JSON.stringify(recentItems)
-            ) {
-              console.log(
-                "The recent_items and active_items_count has not changed."
-              );
-              return Promise.resolve("No need to update");
-            }
-
-            return admin
-              .firestore()
-              .collection("shoppinglists")
-              .doc(context.params.listID)
-              .update({
-                recent_items: recentItems,
-                active_items_count: items.length,
-                modified: admin.firestore.Timestamp.fromDate(new Date()),
-              })
-              .then(() => {
-                return Promise.resolve(
-                  `Wrote recent_items: ${JSON.stringify(recentItems)}`
-                );
-              })
-              .catch((error) => {
-                console.error("Error trying to write recent_items", error);
-                return Promise.reject(error);
-              });
-          })
-          .catch((error) => {
-            console.error("Error getting items snapshot", error);
-            return Promise.reject(error);
-          });
-      })
-      .catch((error) => {
-        console.log("Error getting shopping list:", error);
-        return Promise.reject(error);
+        items.push({
+          text: data.text,
+          description: data.description,
+          added: data.added[0].toDate(),
+          quantity: data.quantity || 0,
+          done: data.done,
+        });
       });
-  });
+      items.sort((a, b) => {
+        if (a.done === b.done) {
+          return b.added.getTime() - a.added.getTime();
+        } else if (a.done) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+
+      const recentItems = items.slice(0, CUTOFF_RECENT_ITEMS).map((item) => {
+        return {
+          text: item.text,
+          description: item.description,
+          quantity: item.quantity,
+          done: item.done,
+        };
+      });
+
+      if (
+        items.length === activeItemsCountBefore &&
+        JSON.stringify(recentItemsBefore) === JSON.stringify(recentItems)
+      ) {
+        functions.logger.info(
+          "The recent_items and active_items_count has not changed."
+        );
+        return;
+      }
+
+      await admin
+        .firestore()
+        .collection("shoppinglists")
+        .doc(context.params.listID)
+        .update({
+          recent_items: recentItems,
+          active_items_count: items.length,
+          modified: admin.firestore.Timestamp.fromDate(new Date()),
+        });
+    })
+  );
 
 interface OwnerMetadata {
   email?: string;
@@ -168,7 +156,7 @@ export const onShoppinglistWriteOwnersMetadata = functions.firestore
       console.log(
         `SAVE new ownersMetadata := ${JSON.stringify(ownersMetadata)}`
       );
-      admin
+      await admin
         .firestore()
         .collection("shoppinglists")
         .doc(context.params.listID)
