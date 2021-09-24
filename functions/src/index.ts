912d46fad747118ea13c91d7da423a4fcf55f022
Peter Bengtsson <mail@peterbe.com>
2021-09-23 21:40:12 -0400

    delete images from storage when deleting items

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,7 @@ export * from "./list-items";
 export * from "./download-and-resize";
 export * from "./image-to-text";
 export * from "./foodwords";
+export * from "./storage-cleanup";
 // Remember, you can't run scheduled jobs in the emulator.
 // See https://stackoverflow.com/a/61253896
 // To test in emulator, write a HTTP function and use
