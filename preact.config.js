b477947ba6c5cd6a99a00f5f284ffc9751d70eb9
Peter Bengtsson <mail@peterbe.com>
2021-10-11 10:33:55 -0400

    preact-cli 3.3.0 upgrade

--- preact.config.js
+++ preact.config.js
@@ -25,25 +25,6 @@ export default {
       },
     });
 
-    // Origins of this is @developit helping me solve this problem:
-    // https://github.com/firebase/firebase-js-sdk/issues/5492
-    const { rule } = helpers.getLoadersByName(config, "babel-loader")[0];
-    const babelConfig = rule.options;
-    const index = babelConfig.plugins.findIndex((x) => {
-      if (Array.isArray(x)) {
-        return /fast-async/.test(x[0]);
-      }
-      if (typeof x === "string") {
-        return /fast-async/.test(x);
-      }
-      console.warn(`Not sure how to deal with type ${typeof x} (${x})`);
-      return false;
-    });
-    if (index !== -1) {
-      // console.debug("Removing babelConfig", babelConfig.plugins[index]);
-      babelConfig.plugins.splice(index, 1);
-    }
-
     // // Use any `index` file, not just index.js
     // config.resolve.alias["preact-cli-entrypoint"] = resolve(
     //   process.cwd(),
